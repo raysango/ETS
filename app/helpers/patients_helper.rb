d3dbc4bbdc71bfedbe8170027da3250eca230e0c
Raymond Morcos <remongeorge@live.com>
2019-08-22 21:06:38 -0700

    Remove unused condition

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -2,7 +2,7 @@ module PatientsHelper
 
   def patient_ets_summary(patient, facility)
     admission = patient.admission
-    return "Summary not available" unless admission.present? &&
+    return "Summary not available" unless admission.present?
     "This #{patient.patient_age} years old #{patient.gender} was admitted to #{facility.try(:name) || 'our'} emergency facility on #{admission.get_moment_text} due to
     #{admission.get_patient_diagnoses_text}. The observed symptoms on admission were #{admission.get_symptoms_text}. #{admission.get_observations_text}.<br/><br/>
     Upon asking about known allergies, the patient disclosed #{patient.get_allergies_text}. Upon asking about chronic conditions,
